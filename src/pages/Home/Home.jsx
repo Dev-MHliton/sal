51e898d7aba8b8c1e5720320adeb409bd96528c2
Dev-MHliton <liton41126nk@gmail.com>
2026-08-03 21:42:17 +0600

    navbar setup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import Navbar from "../../shared/Navbar/Navbar";
 
 const Home = () => {
     return (
         <div>
             <h1>Home page!</h1>
+            <Navbar></Navbar>
         </div>
     );
 };
